7222af6e7665ef06dd266e136560f709612400b9
Gabrielzinx <gabriel.soaressc58@gmail.com>
2023-07-02 13:47:20 -0400

    adding Onboarding path

---
+++ src/pages/Onboarding/OnboardingItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Image, Text, StyleSheet, useWindowDimensions } from 'react-native';
+
+export default function OnboardingItem({ item }) {
+
+    const { width } = useWindowDimensions();
+
+    return (
+        <View style={styles.container}>
+            <Image source={item.image} style={[styles.image, {width: width - 120, resizeMode: 'contain'}]} />
+            <View style={{flex: 0.4}}> 
+                <Text style={[styles.title, {width}]}>{item.title}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    image: {
+        flex: 0.7,
+        justifyContent: 'center',
+        alignSelf: 'center',
+
+    },
+    title: {
+        color: '#FFF',
+        fontWeight: 600,
+        fontSize: 26,
+        textAlign: 'center',
+        paddingHorizontal: 30,
+        marginTop: 20,
+    },
+});
---
+++ src/pages/Onboarding/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useRef } from "react";
+import { View, StyleSheet, FlatList, StatusBar, Animated, SafeAreaView } from 'react-native';
+
+import slides from './slides';
+import OnboardingItem from "./OnboardingItem";
+import NextButton from "../../components/NextButton";
+
+export default function Onboarding({ navigation }) {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const slidesRef = useRef(null);
+
+    const viewableItemsChanged = useRef(({ viewableItems }) => {
+        setCurrentIndex(viewableItems[0].index);
+    }).current;
+
+    const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+    const scrollTo = () => {
+        if (currentIndex < slides.length - 1) {
+            slidesRef.current.scrollToIndex({ index: currentIndex + 1 })
+        } else {
+            console.log('Last item.')
+            navigation.navigate("Login")
+        };
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="light" />
+            <View style={{ flex: 3 }}>
+                <FlatList
+                    data={slides}
+                    renderItem={({ item }) => <OnboardingItem item={item} />}
+                    horizontal
+                    showsHorizontalScrollIndicator
+                    pagingEnabled
+                    bounces={false}
+                    keyExtractor={(item) => item.id}
+                    onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+                        useNativeDriver: false
+                    })}
+                    onViewableItemsChanged={viewableItemsChanged}
+                    viewabilityConfig={viewConfig}
+                    ref={slidesRef}
+                />
+                <NextButton
+                    scrollTo={scrollTo}
+                    percentage={(currentIndex + 1) * (100 / slides.length)}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: '#0A0A0C',
+    }
+});
